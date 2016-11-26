ff4018292df3b60df3b01061271da3bb54dc1bff
frenchman <kujzub@gmail.com>
2016-11-26 15:46:09 +0200

    add level23.lesson13.big01

---
+++ src/main/java/com/javarush/test/level23/lesson13/big01/Room.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level23.lesson13.big01;
+
+/**
+ * Created by frenchman on 26.11.16.
+ */
+public class Room {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/javarush/test/level23/lesson13/big01/Snake.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level23.lesson13.big01;
+
+/**
+ * Created by frenchman on 26.11.16.
+ */
+public class Snake {
+}
