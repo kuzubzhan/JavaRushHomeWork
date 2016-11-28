77fc28fb247ae6967a2cdb24418cf11f9a3bb1b3
frenchman <kujzub@gmail.com>
2016-11-28 14:10:05 +0200

    level22.lesson18.big01_1-3

---
+++ src/main/java/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level22.lesson18.big01;
+
+public class Figure {
+}
---
+++ src/main/java/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level22.lesson18.big01;
+
+public class Tetris {
+    private Field field;
+    private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
