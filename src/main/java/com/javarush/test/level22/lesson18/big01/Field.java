dadfac1cf7e3ba7e655322cf235b5bdf935b758d
frenchman <kujzub@gmail.com>
2016-11-28 14:31:57 +0200

    level22.lesson18.big01_6

--- src/main/java/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/main/java/com/javarush/test/level22/lesson18/big01/Field.java
@@ -1,4 +1,25 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Field(int width, int height, int[][] matrix) {
+        this.width = width;
+        this.height = height;
+        this.matrix = matrix;
+    }
 }
