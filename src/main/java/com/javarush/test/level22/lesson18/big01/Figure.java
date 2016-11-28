76aaf7c144274ca5e9c39e3a269cbf7d34bd4d72
frenchman <kujzub@gmail.com>
2016-11-28 14:40:38 +0200

    level22.lesson18.big01_8

--- src/main/java/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/main/java/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -1,4 +1,25 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
