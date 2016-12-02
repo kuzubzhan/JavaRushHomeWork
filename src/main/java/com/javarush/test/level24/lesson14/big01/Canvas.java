51e8d21cf329455e705c5d577a1c903d8db10027
frenchman <kujzub@gmail.com>
2016-12-03 01:42:19 +0200

    level24.lesson14.big01_10

--- src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -8,7 +8,7 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        matrix = new char[width][height];
+        matrix = new char[height][width];
     }
 
     public int getWidth() {
@@ -21,5 +21,19 @@ public class Canvas {
 
     public char[][] getMatrix() {
         return matrix;
+    }
+
+    public void setPoint(double x, double y, char c) {
+        if (x > 0 && y > 0 && y < matrix.length && x < matrix[0].length) {
+            matrix[(int) y][((int) x)] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) setPoint(x+j, y+i, c);
+            }
+        }
     }
 }
