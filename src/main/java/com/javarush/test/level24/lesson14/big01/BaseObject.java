ed85d415010f2ca4d35a631fb0cb5503510dd48b
frenchman <kujzub@gmail.com>
2016-12-02 23:29:57 +0200

    level24.lesson14.big01_9

--- src/main/java/com/javarush/test/level24/lesson14/big01/BaseObject.java
+++ src/main/java/com/javarush/test/level24/lesson14/big01/BaseObject.java
@@ -1,46 +1,83 @@
 package com.javarush.test.level24.lesson14.big01;
 
-public abstract class BaseObject {
-    double x;
-    double y;
-    double radius;
+/**
+ * Базовый класс для всех объектов игры.
+ */
+public abstract class BaseObject
+{
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
 
-    public BaseObject(double x, double y, double radius) {
+    protected BaseObject(double x, double y, double radius)
+    {
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
 
-    public double getX() {
+    public double getX()
+    {
         return x;
     }
 
-    public void setX(double x) {
+    public void setX(double x)
+    {
         this.x = x;
     }
 
-    public double getY() {
+    public double getY()
+    {
         return y;
     }
 
-    public void setY(double y) {
+    public void setY(double y)
+    {
         this.y = y;
     }
 
-    public double getRadius() {
+    public double getRadius()
+    {
         return radius;
     }
 
-    public void setRadius(double radius) {
+    public void setRadius(double radius)
+    {
         this.radius = radius;
     }
 
-    public abstract void draw();
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public abstract void draw(Canvas canvas);
 
+    /**
+     * Двигаем себя на один ход.
+     */
     public abstract void move();
 
-    public boolean isIntersec(BaseObject o) {
-        double dist = Math.sqrt(Math.pow(x - o.getX(), 2) + Math.pow(y - o.getY(), 2));
-        return dist < Math.max(radius, o.getRadius());
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy)
+    {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    public boolean isIntersec(BaseObject o)
+    {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
--- src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -1,4 +1,25 @@
 package com.javarush.test.level24.lesson14.big01;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[width][height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
