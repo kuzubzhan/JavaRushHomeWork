229030939ce0bf4bb0b7b404e02a0a52a069d25c
frenchman <kujzub@gmail.com>
2016-12-03 02:03:19 +0200

    level24.lesson14.big01_11

--- src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/main/java/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -36,4 +36,19 @@ public class Canvas {
             }
         }
     }
+
+    public void clear() {
+        matrix = new char[height][width];
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(" " + matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
