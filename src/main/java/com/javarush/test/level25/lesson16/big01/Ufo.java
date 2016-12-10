c3c18f774b1791320fd08ec97e8dd7f2d4c4176f
frenchman <kujzub@gmail.com>
2016-12-11 01:38:02 +0200

    level25.lesson16.big01_15

--- src/main/java/com/javarush/test/level25/lesson16/big01/Ufo.java
+++ src/main/java/com/javarush/test/level25/lesson16/big01/Ufo.java
@@ -1,7 +1,34 @@
 package com.javarush.test.level25.lesson16.big01;
 
 public class Ufo extends BaseObject {
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'U');
+    }
+
+    public void move() {
+        double dx = Math.random() * 2 - 1;  //-1..1
+        double dy = Math.random() * 2 - 1;  //-1..1
+        y += dy;
+        x += dx;
+        checkBorders(radius, Space.game.getWidth()-radius, radius, Space.game.getHeight()/2 - radius);
+        int toFire = (int) (Math.random() * 10);
+        if (toFire == 0) fire();
+    }
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y));
     }
 }
