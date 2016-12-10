a338c624e1991e45381c68b951014e8e97917e38
frenchman <kujzub@gmail.com>
2016-12-10 23:48:56 +0200

    level25.lesson16.big01_13

--- src/main/java/com/javarush/test/level25/lesson16/big01/Rocket.java
+++ src/main/java/com/javarush/test/level25/lesson16/big01/Rocket.java
@@ -1,15 +1,29 @@
 package com.javarush.test.level25.lesson16.big01;
 
-public class Rocket extends BaseObject {
-    public Rocket(double x, double y) {
+/**
+ * Класс для объектов-ракета
+ */
+public class Rocket  extends BaseObject
+{
+
+    public Rocket(double x, double y)
+    {
         super(x, y, 1);
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
     public void draw(Canvas canvas)
     {
         canvas.setPoint(x,y,'R');
     }
 
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
     public void move()
     {
         y--;
--- src/main/java/com/javarush/test/level25/lesson16/big01/SpaceShip.java
+++ src/main/java/com/javarush/test/level25/lesson16/big01/SpaceShip.java
@@ -1,7 +1,17 @@
 package com.javarush.test.level25.lesson16.big01;
 
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx = 0.0;
+
+    public SpaceShip(int x, int y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft() {
+        this.dx = -1;
+    }
+
+    public void moveRight() {
+        this.dx = 1;
     }
 }
