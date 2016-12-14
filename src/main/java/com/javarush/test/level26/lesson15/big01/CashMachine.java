eb9b17f174efcaaf6072dee0ac8a1a5fc1965224
frenchman <kujzub@gmail.com>
2016-12-14 06:20:02 +0200

    level26.lesson15.big01_4

--- src/main/java/com/javarush/test/level26/lesson15/big01/CashMachine.java
+++ src/main/java/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -1,7 +1,10 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
 
     }
 }
--- src/main/java/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/main/java/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -19,4 +19,30 @@ public class ConsoleHelper {
         }
         return result;
     }
+
+    public static String askCurrencyCode() {
+        String result;
+        writeMessage("Enter code");
+        while ((result = readString()).length() != 3) writeMessage("Mistake");
+
+        return result.toUpperCase();
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        String[] strings;
+        writeMessage("Enter two integer");
+
+        while (true) {
+            strings = readString().split("\\s");
+            try {
+                if (strings.length == 2 && Integer.parseInt(strings[0]) > 0 && Integer.parseInt(strings[1]) > 0)
+                    break;
+                writeMessage("Mistake of data");
+            } catch (Exception e) {
+                writeMessage("Mistake of data and Exception");
+            }
+        }
+
+        return strings;
+    }
 }
