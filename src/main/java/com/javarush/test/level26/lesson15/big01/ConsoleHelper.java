ab82d11fa033e7dae59786ac19c29602ada899f5
frenchman <kujzub@gmail.com>
2016-12-14 02:31:25 +0200

    level26.lesson15.big01_1-3

---
+++ src/main/java/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level26.lesson15.big01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String result = "";
+        try {
+            result = br.readLine();
+        } catch (IOException e) {
+        }
+        return result;
+    }
+}
