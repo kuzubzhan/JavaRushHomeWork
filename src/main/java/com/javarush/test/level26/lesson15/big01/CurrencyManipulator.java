ddaff66345c2234a8d779e6a92b9dee7c9f6e924
frenchman <kujzub@gmail.com>
2017-01-19 02:47:30 +0200

    level26.lesson15.big01_10

--- src/main/java/com/javarush/test/level26/lesson15/big01/CurrencyManipulator.java
+++ src/main/java/com/javarush/test/level26/lesson15/big01/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level26.lesson15.big01;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.javarush.test.level26.lesson15.big01.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -31,5 +32,53 @@ public class CurrencyManipulator {
 
     public boolean hasMoney() {
         return denominations.size() != 0;
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int amount = expectedAmount;
+
+        Map<Integer, Integer> result = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        Map<Integer, Integer> temp = new HashMap<>(denominations);
+
+        List<Integer> list = new ArrayList<>(temp.keySet());
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        for (Integer arr : list) {
+            int key = arr;
+            int value = temp.get(key);
+            while (true) {
+                if (key > amount || value == 0) {
+                    temp.put(key, value);
+                    break;
+                }
+                amount = amount - key;
+                value--;
+                if (result.containsKey(key)) result.put(key, result.get(key) + 1);
+                else result.put(key, 1);
+            }
+        }
+
+        if (amount > 0) throw new NotEnoughMoneyException();
+        else {
+            for (Map.Entry<Integer, Integer> pair : result.entrySet()) {
+                ConsoleHelper.writeMessage("\t" + pair.getKey() + " - " + pair.getValue());
+            }
+            denominations.clear();
+            denominations.putAll(temp);
+            ConsoleHelper.writeMessage("Transaction Ok");
+        }
+
+        return result;
     }
 }
--- src/main/java/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
+++ src/main/java/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
@@ -1,8 +1,39 @@
 package com.javarush.test.level26.lesson15.big01.command;
 
+import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulator;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulatorFactory;
+import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+import com.javarush.test.level26.lesson15.big01.exception.NotEnoughMoneyException;
+
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        String code = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator cm = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
+        while (true) {
+            ConsoleHelper.writeMessage("Enter amount");
+            String str = ConsoleHelper.readString();
+            try {
+                int amount = Integer.parseInt(str);
 
+                if (amount <= 0) {
+                    ConsoleHelper.writeMessage("Amount <= 0");
+                    continue;
+                }
+
+                if (! cm.isAmountAvailable(amount)) {
+                    ConsoleHelper.writeMessage("Amount to mach");
+                    continue;
+                }
+
+                cm.withdrawAmount(amount);
+                break;
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("NumberFormatException");
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("NotEnoughMoneyException");
+            }
+        }
     }
 }
