3cde142c7f940be4a2512573df00d26fbc211fd1
frenchman <kujzub@gmail.com>
2017-02-03 00:20:16 +0200

    level27.lesson15.big01_3

--- src/main/java/com/javarush/test/level27/lesson15/big01/Tablet.java
+++ src/main/java/com/javarush/test/level27/lesson15/big01/Tablet.java
@@ -3,10 +3,11 @@ package com.javarush.test.level27.lesson15.big01;
 import com.javarush.test.level27.lesson15.big01.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable {
     private final int number;
     static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -19,6 +20,8 @@ public class Tablet {
         try {
             order = new Order(this);
             ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
---
+++ src/main/java/com/javarush/test/level27/lesson15/big01/kitchen/Cook.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level27.lesson15.big01.kitchen;
+
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Cook implements Observer {
+    private final String name;
+
+    public Cook(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void update(Observable observable, Object arg) {
+        ConsoleHelper.writeMessage("Start cooking - " + arg);
+    }
+}
