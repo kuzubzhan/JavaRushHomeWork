5433bdee9ec8ab5b471ca7e25ac2c7f7a7b60dbe
frenchman <kujzub@gmail.com>
2017-03-03 23:34:30 +0200

    level29.lesson15.big01_7

--- src/main/java/com/javarush/test/level29/lesson15/big01/human/Student.java
+++ src/main/java/com/javarush/test/level29/lesson15/big01/human/Student.java
@@ -20,16 +20,16 @@ public class Student extends UniversityPerson {
     public void learn() {
     }
 
-    public void incAverageGradeBy01() {
-        averageGrade += 0.1;
+    public void incAverageGrade(double delta) {
+        averageGrade += delta;
     }
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
