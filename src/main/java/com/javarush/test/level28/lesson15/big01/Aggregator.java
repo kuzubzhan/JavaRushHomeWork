9d0f6d4aed5f05d980c1f6f67d75a091f710e0d9
frenchman <kujzub@gmail.com>
2017-03-05 01:22:40 +0200

    level28.lesson15.big01_12

--- src/main/java/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/main/java/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,6 +1,17 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
+import com.javarush.test.level28.lesson15.big01.model.Model;
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.view.HtmlView;
+
 public class Aggregator {
     public static void main(String[] args) {
+        HtmlView view = new HtmlView();
+        Provider[] provider = new Provider[]{new Provider(new HHStrategy())};
+        Model model = new Model(view, provider);
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
     }
 }
