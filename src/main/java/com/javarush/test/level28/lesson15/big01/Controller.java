183a019e34e3408854b78311c8e5cc81588a57f9
frenchman <kujzub@gmail.com>
2017-03-05 00:30:12 +0200

    level28.lesson15.big01_11

--- src/main/java/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/main/java/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,33 +1,17 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0)
+    public Controller(Model model) {
+        if (model == null)
             throw new IllegalArgumentException();
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            vacancies.addAll(provider.getJavaVacancies(""));
-        }
-        System.out.println(vacancies.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
