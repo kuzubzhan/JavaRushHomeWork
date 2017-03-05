d81046f75506c70a0ed2afeaaab9cbf08334846d
frenchman <kujzub@gmail.com>
2017-03-05 02:36:09 +0200

    level28.lesson15.big01_13

--- src/main/java/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/main/java/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public class HtmlView implements View {
     private Controller controller;
+    private final String filePath = "./src/" + this.getClass().getPackage().getName().replace(".", "/") + "/vacancies.html";
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        updateFile(getUpdatedFileContent(vacancies));
     }
 
     @Override
@@ -19,5 +20,13 @@ public class HtmlView implements View {
 
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        return null;
+    }
+
+    private void updateFile(String s) {
+
     }
 }
