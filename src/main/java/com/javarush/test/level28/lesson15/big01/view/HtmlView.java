9e5360b2bfeb8153eb79c70d04633e34a6766011
frenchman <kujzub@gmail.com>
2017-03-10 23:05:15 +0200

    level28.lesson15.big01_15

--- src/main/java/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/main/java/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -2,8 +2,12 @@ package com.javarush.test.level28.lesson15.big01.view;
 
 import com.javarush.test.level28.lesson15.big01.Controller;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -26,7 +30,33 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return null;
+        Document doc;
+        try {
+            doc = getDocument();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+
+        Element templateElem = doc.select("[class~=template]").first();
+
+        Element pattern = templateElem.clone();
+        pattern.removeAttr("style");
+        pattern.removeClass("template");
+
+        doc.select("tr[class=vacancy]").remove();
+
+        for (Vacancy vacancy : vacancies) {
+            Element newVacancyElement = pattern.clone();
+
+            newVacancyElement.select("[class=city]").first().text(vacancy.getCity());
+            newVacancyElement.select("[class=companyName]").first().text(vacancy.getCompanyName());
+            newVacancyElement.select("[class=salary]").first().text(vacancy.getSalary());
+            newVacancyElement.select("a").first().text(vacancy.getTitle()).attr("href" , vacancy.getUrl());
+
+            templateElem.before(newVacancyElement.outerHtml());
+        }
+        return doc.html();
     }
 
     private void updateFile(String newContent) {
@@ -37,5 +67,9 @@ public class HtmlView implements View {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
