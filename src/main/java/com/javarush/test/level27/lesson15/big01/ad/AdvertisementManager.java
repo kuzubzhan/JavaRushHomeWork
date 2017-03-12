d6c530045630e53614e20008a2e34f456c6d7d0b
frenchman <kujzub@gmail.kom>
2017-03-13 01:58:29 +0200

    level27.lesson15.big01_8

--- src/main/java/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
+++ src/main/java/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
@@ -1,5 +1,16 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos() {
+        ConsoleHelper.writeMessage("processVideos method calling");
+    }
 }
