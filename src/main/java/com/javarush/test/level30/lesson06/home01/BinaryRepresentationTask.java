9d94fd3fbc887a37a04db2e591b5313803f1eec3
frenchman <kujzub@gmail.kom>
2017-03-16 14:18:34 +0200

    level30.lesson06.home01

---
+++ src/main/java/com/javarush/test/level30/lesson06/home01/BinaryRepresentationTask.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level30.lesson06.home01;
+
+import java.util.concurrent.RecursiveTask;
+
+public class BinaryRepresentationTask extends RecursiveTask<String> {
+    private int i;
+
+    public BinaryRepresentationTask(int i) {
+        this.i = i;
+    }
+
+    @Override
+    protected String compute() {
+        int a = i % 2;
+        int b = i / 2;
+        String result = String.valueOf(a);
+        if (b > 0) {
+            BinaryRepresentationTask task = new BinaryRepresentationTask(b);
+            task.fork();
+            result = task.join() + result;
+        }
+        return result;
+    }
+}
