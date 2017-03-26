0bd106290fdd9a025b388d2a72f06e3c3fee0908
frenchman <kujzub@gmail.kom>
2017-03-26 07:49:36 +0300

    level30.lesson15.big01_6

--- src/main/java/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/main/java/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,4 +1,27 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static class Handler extends Thread {
+        private Socket socket;
+        private Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Input port server");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket socket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Server is started");
+            while (true) {
+                new Handler(socket.accept()).start();
+            }
+        }
+        catch (Exception e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
 }
