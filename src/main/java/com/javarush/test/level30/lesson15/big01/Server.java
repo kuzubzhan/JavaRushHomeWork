d578fb0a9e625a7289dd4081c248c771269959f6
frenchman <kujzub@gmail.kom>
2017-03-26 15:52:04 +0300

    level30.lesson15.big01_7

--- src/main/java/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/main/java/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,13 +1,28 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     private static class Handler extends Thread {
         private Socket socket;
         private Handler(Socket socket) {
             this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Message is not sent");
+            }
         }
     }
 
