201711f1e3762b62cdd82ba711bc70b7c87bdeba
frenchman <kujzub@gmail.kom>
2017-03-28 08:27:54 +0300

    level30.lesson15.big01_18

---
+++ src/main/java/com/javarush/test/level30/lesson15/big01/client/BotClient.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+public class BotClient extends Client {
+    public class BotSocketThread extends SocketThread {
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSentTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + (int) (Math.random() * 100);
+    }
+
+    public static void main(String[] args) {
+        new BotClient().run();
+    }
+}
