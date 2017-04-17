88f72a7fe2a93d62e45d34483badbb8669ee81fc
frenchman <kujzub@gmail.kom>
2017-04-18 02:25:54 +0300

    level31.lesson15.big01_3

---
+++ src/main/java/com/javarush/test/level31/lesson15/big01/Operation.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level31.lesson15.big01;
+
+public enum Operation {
+    CREATE,  // Создать архив
+    ADD,     // Добавить файл в архив
+    REMOVE,  // Удалить файл из архива
+    EXTRACT, // Извлечь содержимое архива
+    CONTENT, // Просмотреть содержимое архива
+    EXIT     // Выйти из программы
+}
