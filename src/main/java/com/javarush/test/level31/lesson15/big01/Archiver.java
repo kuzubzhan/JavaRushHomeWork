df9d8106bfdee85c55b26bb11245fbd404322f39
frenchman <kujzub@gmail.kom>
2017-04-17 18:29:40 +0300

    level31.lesson15.big01_1

---
+++ src/main/java/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level31.lesson15.big01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter path to zipFile");
+        Path path = Paths.get(br.readLine());
+        ZipFileManager fileManager = new ZipFileManager(path);
+
+        System.out.println("Enter path to file");
+        path = Paths.get(br.readLine());
+        fileManager.createZip(path);
+    }
+}
