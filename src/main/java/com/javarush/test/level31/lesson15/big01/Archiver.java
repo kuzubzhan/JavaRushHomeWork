edd521bf50a86c05c6cf0e9f6a6fa25eb21e40fe
frenchman <kujzub@gmail.kom>
2017-04-18 21:33:08 +0300

    level31.lesson15.big01_8

--- src/main/java/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/main/java/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,22 +1,45 @@
 package com.javarush.test.level31.lesson15.big01;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
+
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            Operation operation = null;
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        System.out.println("Enter path to zipFile");
-        Path path = Paths.get(br.readLine());
-        ZipFileManager fileManager = new ZipFileManager(path);
+            if (operation == Operation.EXIT) break;
+        }
+    }
 
-        System.out.println("Enter path to file");
-        path = Paths.get(br.readLine());
-        fileManager.createZip(path);
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                "\t" + Operation.CREATE.ordinal()  + " - упаковать файлы в архив\n" +
+                "\t" + Operation.ADD.ordinal()     + " - добавить файл в архив\n" +
+                "\t" + Operation.REMOVE.ordinal()  + " - удалить файл из архива\n" +
+                "\t" + Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                "\t" + Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                "\t" + Operation.EXIT.ordinal()    + " – выход");
 
-        CommandExecutor.execute(Operation.EXIT);
+        int operationNumber = ConsoleHelper.readInt();
+
+        Operation result = null;
+        if (operationNumber == Operation.CREATE.ordinal()) result = Operation.CREATE;
+        else if (operationNumber == Operation.ADD.ordinal()) result = Operation.ADD;
+        else if (operationNumber == Operation.REMOVE.ordinal()) result = Operation.REMOVE;
+        else if (operationNumber == Operation.EXTRACT.ordinal()) result = Operation.EXTRACT;
+        else if (operationNumber == Operation.CONTENT.ordinal()) result = Operation.CONTENT;
+        else if (operationNumber == Operation.EXIT.ordinal()) result = Operation.EXIT;
+
+        return result;
     }
 }
