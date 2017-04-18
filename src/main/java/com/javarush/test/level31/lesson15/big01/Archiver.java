a198486e8a1e12653225bba76566aa16630870a1
frenchman <kujzub@gmail.kom>
2017-04-18 23:58:36 +0300

    level31.lesson15.big01_9

--- src/main/java/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/main/java/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -5,9 +5,10 @@ import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
 import java.io.IOException;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-        while (true) {
-            Operation operation = null;
+    public static void main(String[] args) throws IOException {
+
+        Operation operation = null;
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
@@ -17,29 +18,20 @@ public class Archiver {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
 
-            if (operation == Operation.EXIT) break;
-        }
+        } while (operation != Operation.EXIT);
     }
 
+
     public static Operation askOperation() throws IOException {
-        ConsoleHelper.writeMessage("Выберите операцию:\n" +
-                "\t" + Operation.CREATE.ordinal()  + " - упаковать файлы в архив\n" +
-                "\t" + Operation.ADD.ordinal()     + " - добавить файл в архив\n" +
-                "\t" + Operation.REMOVE.ordinal()  + " - удалить файл из архива\n" +
-                "\t" + Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
-                "\t" + Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
-                "\t" + Operation.EXIT.ordinal()    + " – выход");
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
 
-        int operationNumber = ConsoleHelper.readInt();
-
-        Operation result = null;
-        if (operationNumber == Operation.CREATE.ordinal()) result = Operation.CREATE;
-        else if (operationNumber == Operation.ADD.ordinal()) result = Operation.ADD;
-        else if (operationNumber == Operation.REMOVE.ordinal()) result = Operation.REMOVE;
-        else if (operationNumber == Operation.EXTRACT.ordinal()) result = Operation.EXTRACT;
-        else if (operationNumber == Operation.CONTENT.ordinal()) result = Operation.CONTENT;
-        else if (operationNumber == Operation.EXIT.ordinal()) result = Operation.EXIT;
-
-        return result;
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
---
+++ src/main/java/com/javarush/test/level31/lesson15/big01/FileManager.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level31.lesson15.big01;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    private Path rootPath;
+    private List<Path> fileList;
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        this.fileList = new ArrayList<>();
+        collectFileList(rootPath);
+    }
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
+    private void collectFileList(Path path) throws IOException {
+        if (Files.isRegularFile(path)) {
+            fileList.add(rootPath.relativize(path));
+        }
+        else if (Files.isDirectory(path)) {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+            for (Path childPath : stream) {
+                collectFileList(childPath);
+            }
+            stream.close();
+        }
+    }
+}
