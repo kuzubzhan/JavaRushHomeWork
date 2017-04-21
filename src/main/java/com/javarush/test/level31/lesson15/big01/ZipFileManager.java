f362c353288b718929267d5677f9fd4fb41c5349
frenchman <kujzub@gmail.kom>
2017-04-21 20:02:07 +0300

    level31.lesson15.big01_11

--- src/main/java/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/main/java/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -2,8 +2,6 @@ package com.javarush.test.level31.lesson15.big01;
 
 import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -13,48 +11,62 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
-    private Path zipFile;
+    // Полный путь zip файла
+    private final Path zipFile;
 
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
     }
 
     public void createZip(Path source) throws Exception {
-        if (!zipFile.getParent().toFile().exists()) {
-            Files.createDirectories(zipFile.getParent());
-        }
-        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-            if (Files.isRegularFile(source)) {
-                addNewZipEntry(zos, source.getParent(), source.getFileName());
-            }
-            else if (Files.isDirectory(source)) {
+        // Проверяем, существует ли директория, где будет создаваться архив
+        // При необходимости создаем ее
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory))
+            Files.createDirectories(zipDirectory);
+
+        // Создаем zip поток
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+
+            if (Files.isDirectory(source)) {
+                // Если архивируем директорию, то нужно получить список файлов в ней
                 FileManager fileManager = new FileManager(source);
                 List<Path> fileNames = fileManager.getFileList();
-                for (Path fileName : fileNames) {
-                    addNewZipEntry(zos, source, fileName);
-                }
-            }
-            else {
+
+                // Добавляем каждый файл в архив
+                for (Path fileName : fileNames)
+                    addNewZipEntry(zipOutputStream, source, fileName);
+
+            } else if (Files.isRegularFile(source)) {
+
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else {
+
+                // Если переданный source не директория и не файл, бросаем исключение
                 throw new PathIsNotFoundException();
             }
         }
     }
 
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
-        File path = new File(filePath.toString() + File.separator + fileName.toString());
-        try (InputStream is = new FileInputStream(path)) {
-            ZipEntry zipEntry = new ZipEntry(fileName.toString());
-            zipOutputStream.putNextEntry(zipEntry);
-            copyData(is, zipOutputStream);
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry entry = new ZipEntry(fileName.toString());
+
+            zipOutputStream.putNextEntry(entry);
+
+            copyData(inputStream, zipOutputStream);
+
             zipOutputStream.closeEntry();
         }
     }
 
     private void copyData(InputStream in, OutputStream out) throws Exception {
-        int count;
-        byte[] buff = new byte[1024];
-        while ((count = in.read(buff)) != -1) {
-            out.write(buff, 0, count);
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
         }
     }
 }
--- src/main/java/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ src/main/java/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -1,4 +1,17 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введи полный путь файла архива");
+        String str = ConsoleHelper.readString();
+        Path path = Paths.get(str);
+
+        return new ZipFileManager(path);
+    }
 }
