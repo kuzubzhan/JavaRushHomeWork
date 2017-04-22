0d02b59a4070df6ee031692f9636e29d0e607c89
frenchman <kujzub@gmail.kom>
2017-04-22 03:10:52 +0300

    level31.lesson15.big01_12

---
+++ src/main/java/com/javarush/test/level31/lesson15/big01/FileProperties.java
@@ -0,0 +1,59 @@
+package com.javarush.test.level31.lesson15.big01;
+
+public class FileProperties {
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public void setCompressedSize(long compressedSize) {
+        this.compressedSize = compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+
+    public void setCompressionMethod(int compressionMethod) {
+        this.compressionMethod = compressionMethod;
+    }
+    public long getCompressionRatio() {
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        String pattern = "%s %d Kb (%d Kb) сжатие: %d%%";
+        if (size > 0)
+            return String.format(pattern, name, size / 1024, compressedSize / 1024, getCompressionRatio());
+        else
+            return name;
+    }
+}
--- src/main/java/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ src/main/java/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -7,11 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
-    public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введи полный путь файла архива");
-        String str = ConsoleHelper.readString();
-        Path path = Paths.get(str);
 
-        return new ZipFileManager(path);
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
