06f5880a951ef9a206437a4fe832868bce3fe42f
frenchman <kujzub@gmail.kom>
2017-04-28 23:33:58 +0300

    level32.lesson08.home01

---
+++ src/main/java/com/javarush/test/level32/lesson08/home01/CustomInvocationHandler.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level32.lesson08.home01;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+    private SomeInterfaceWithMethods original;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods original) {
+        this.original = original;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(method.getName() + " in");
+        Object o = method.invoke(original, args);
+        System.out.println(method.getName() + " out");
+
+        return o;
+    }
+}
