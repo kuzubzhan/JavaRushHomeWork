67a2e618c7a7805909817fc669a32cd95ca2ac67
frenchman <kujzub@gmail.kom>
2017-05-01 02:41:59 +0300

    level32.lesson15.big01_6

--- src/main/java/com/javarush/test/level32/lesson15/big01/View.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/View.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -46,7 +48,20 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
 
+        JScrollPane htmlScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", htmlScrollPane);
+
+        JScrollPane textScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", textScrollPane);
+        tabbedPane.setPreferredSize(new Dimension(200, 200));
+
+        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(listener);
+
+        Container container = getContentPane();
+        container.add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void selectedTabChanged() {
