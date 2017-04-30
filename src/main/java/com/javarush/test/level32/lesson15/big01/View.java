db00964962d65ec1e450cdaf449ca0de84e7a2ff
frenchman <kujzub@gmail.kom>
2017-04-30 16:41:53 +0300

    level32.lesson15.big01_4

--- src/main/java/com/javarush/test/level32/lesson15/big01/View.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/View.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level32.lesson15.big01;
 
+import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,10 +26,26 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
-
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
     }
 
     public void exit() {
         controller.exit();
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
     }
 }
