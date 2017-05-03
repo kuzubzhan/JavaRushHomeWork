f49e5fc0f3df2fb7a0232607d9f0bf59da7e2d07
frenchman <kujzub@gmail.kom>
2017-05-03 19:44:06 +0300

    level32.lesson15.big01_12

--- src/main/java/com/javarush/test/level32/lesson15/big01/actions/UndoAction.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/actions/UndoAction.java
@@ -13,7 +13,7 @@ public class UndoAction extends AbstractAction {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        view.undo();
     }
 }
