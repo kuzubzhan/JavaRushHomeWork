27a35de71c3aee58ce50fa4d6980df309e995099
frenchman <kujzub@gmail.kom>
2017-05-04 02:01:49 +0300

    level32.lesson15.big01_20

--- src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
     }
 
     public void init() {
-
+        createNewDocument();
     }
 
     public void exit() {
@@ -67,6 +67,11 @@ public class Controller {
     }
 
     public void createNewDocument() {
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML редактор");
+        view.resetUndo();
+        currentFile = null;
     }
 
     public void openDocument() {
