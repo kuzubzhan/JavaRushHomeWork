8ab2f2a09beb35c46d3577d0c760a8a7250043cb
frenchman <kujzub@gmail.kom>
2017-05-04 00:36:32 +0300

    level32.lesson15.big01_17

--- src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -4,6 +4,7 @@ import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -52,5 +53,16 @@ public class Controller {
         } catch (Exception e) {
             ExceptionHandler.log(e);
         }
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+
+        return writer.toString();
     }
 }
