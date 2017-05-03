aa870ed111ca274858ae6d33922efad061d2dd8c
frenchman <kujzub@gmail.kom>
2017-05-03 18:40:45 +0300

    level32.lesson15.big01_10

--- src/main/java/com/javarush/test/level32/lesson15/big01/View.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/View.java
@@ -85,4 +85,12 @@ public class View extends JFrame implements ActionListener {
     public void selectedTabChanged() {
 
     }
+
+    public boolean canUndo() {
+        return false;
+    }
+
+    public boolean canRedo() {
+        return false;
+    }
 }
