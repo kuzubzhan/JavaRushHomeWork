1d9bd2da3382b5ef3c496f8fd5a6f8d755df0e20
frenchman <kujzub@gmail.kom>
2017-05-05 01:26:06 +0300

    level32.lesson15.big01_21

---
+++ src/main/java/com/javarush/test/level32/lesson15/big01/HTMLFileFilter.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        boolean result = false;
+        String fileName = f.getName();
+        if (f.isDirectory() || fileName.toLowerCase().endsWith(".html") || fileName.toLowerCase().endsWith(".htm")) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
