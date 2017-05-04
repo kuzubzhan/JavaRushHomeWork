053a0b8856c5c06d6e9a35f491c0ba906c521e0d
frenchman <kujzub@gmail.kom>
2017-05-05 02:52:56 +0300

    level32.lesson15.big01_23-24

--- src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -3,10 +3,7 @@ package com.javarush.test.level32.lesson15.big01;
 import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Controller {
     private View view;
@@ -77,9 +74,40 @@ public class Controller {
     }
 
     public void openDocument() {
+        try {
+            view.selectHtmlTab();
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileFilter(new HTMLFileFilter());
+            int answer = chooser.showOpenDialog(view);
+
+            if (answer == JFileChooser.APPROVE_OPTION) {
+                currentFile = chooser.getSelectedFile();
+                resetDocument();
+                view.setTitle(currentFile.getName());
+                try (FileReader reader = new FileReader(currentFile)) {
+                    new HTMLEditorKit().read(reader, document, 0);
+                }
+                view.resetUndo();
+            }
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
     }
 
     public void saveDocument() {
+        if (currentFile == null) {
+            saveDocumentAs();
+        }
+        else {
+            try {
+                view.selectHtmlTab();
+                try (FileWriter writer = new FileWriter(currentFile)) {
+                    new HTMLEditorKit().write(writer, document, 0, document.getLength());
+                }
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
     }
 
     public void saveDocumentAs() {
