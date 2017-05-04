25cf03a2f14818830d7ce0822edc84ae9439c099
frenchman <kujzub@gmail.kom>
2017-05-05 02:30:26 +0300

    level32.lesson15.big01_22

--- src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/main/java/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -81,5 +83,21 @@ public class Controller {
     }
 
     public void saveDocumentAs() {
+        try {
+            view.selectHtmlTab();
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileFilter(new HTMLFileFilter());
+            int answer = chooser.showSaveDialog(view);
+
+            if (answer == JFileChooser.APPROVE_OPTION) {
+                currentFile = chooser.getSelectedFile();
+                view.setTitle(currentFile.getName());
+                try (FileWriter writer = new FileWriter(currentFile)) {
+                    new HTMLEditorKit().write(writer, document, 0, document.getLength());
+                }
+            }
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
     }
 }
