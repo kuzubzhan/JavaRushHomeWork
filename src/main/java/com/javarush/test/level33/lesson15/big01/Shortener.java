f819071b5ef4ced50bc351361cbded25fe50ec50
frenchman <kujzub@gmail.kom>
2017-05-09 02:15:13 +0300

    level33.lesson15.big01_1

---
+++ src/main/java/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level33.lesson15.big01;
+
+public class Shortener {
+    public Long getId(String string) {
+        return null;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
