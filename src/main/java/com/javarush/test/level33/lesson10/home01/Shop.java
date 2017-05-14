87991403632a5cef5c49ab025dfcf33a39de0372
frenchman <kujzub@gmail.kom>
2017-05-14 23:24:31 +0300

    level33.lesson10.home01

---
+++ src/main/java/com/javarush/test/level33/lesson10/home01/Shop.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level33.lesson10.home01;
+
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlType(name = "shop")
+@XmlRootElement
+public class Shop {
+    @XmlElementWrapper(name = "goods")
+    public List<String> names = new ArrayList<>();
+
+    public int count;
+    public double profit;
+    public List<String> secretData = new ArrayList<>();
+
+    public Shop() {
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "names=" + names +
+                ", count=" + count +
+                ", profit=" + profit +
+                ", secretData=" + secretData +
+                '}';
+    }
+}
