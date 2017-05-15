da185a754877ec1b26e4396f61f25f64eef95498
frenchman <kujzub@gmail.kom>
2017-05-15 22:13:33 +0300

    level34.lesson15.big01_1

---
+++ src/main/java/com/javarush/test/level34/lesson15/big01/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level34.lesson15.big01.controller;
+
+import com.javarush.test.level34.lesson15.big01.model.Model;
+import com.javarush.test.level34.lesson15.big01.view.View;
+
+public class Controller {
+    private View view;
+    private Model model;
+
+    public Controller() {
+        view = new View(this);
+        model = new Model();
+    }
+}
---
+++ src/main/java/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level34.lesson15.big01.model;
+
+public class Model {
+}
