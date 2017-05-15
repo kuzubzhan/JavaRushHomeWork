32796c78ee38fddeeaa71aa8e50f63f9c1d79fde
frenchman <kujzub@gmail.kom>
2017-05-15 22:55:09 +0300

    level34.lesson15.big01_3

---
+++ src/main/java/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level34.lesson15.big01.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JPanel {
+    private View view;
+
+    public Field(View view) {
+        this.view = view;
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
