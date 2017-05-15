ff352c7e64ad7aca8b5414172b255547d4d9d232
frenchman <kujzub@gmail.kom>
2017-05-16 02:18:48 +0300

    level34.lesson15.big01_9

--- src/main/java/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/main/java/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -1,5 +1,12 @@
 package com.javarush.test.level34.lesson15.big01.model;
 
+import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+
 public class Model {
     public static final int FIELD_SELL_SIZE = 20;
+    private EventListener eventListener;
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
+    }
 }
--- src/main/java/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/main/java/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,10 +1,13 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
+import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Field extends JPanel {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
@@ -12,5 +15,9 @@ public class Field extends JPanel {
 
     public void paint(Graphics g) {
 
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 }
