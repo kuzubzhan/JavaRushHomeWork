f24da5bda5867b34c0f15ae2e2710577408c9b57
frenchman <kujzub@gmail.kom>
2017-05-17 02:58:00 +0300

    level34.lesson15.big01_15

--- src/main/java/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/main/java/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -33,6 +33,83 @@ public class Model {
     }
 
     public void move(Direction direction) {
+        Player player = gameObjects.getPlayer();
+        if (checkWallCollision(player, direction))
+            return;
+        if (checkBoxCollision(direction))
+            return;
 
+        switch (direction) {
+            case LEFT:
+                player.move(-FIELD_SELL_SIZE, 0);
+                break;
+            case RIGHT:
+                player.move(FIELD_SELL_SIZE, 0);
+                break;
+            case UP:
+                player.move(0, -FIELD_SELL_SIZE);
+                break;
+            case DOWN:
+                player.move(0, FIELD_SELL_SIZE);
+                break;
+        }
+
+        checkCompletion();
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (Wall wall : gameObjects.getWalls()) {
+            if (gameObject.isCollision(wall, direction))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollision(Direction direction) {
+        Player player = gameObjects.getPlayer();
+        for (Box box : gameObjects.getBoxes()) {
+            if (player.isCollision(box, direction)) {
+
+                for (Box box1 : gameObjects.getBoxes()) {
+                    if (box.isCollision(box1, direction))
+                        return true;
+                }
+
+                if (checkWallCollision(box, direction))
+                    return true;
+
+                switch (direction) {
+                    case LEFT:
+                        box.move(-FIELD_SELL_SIZE, 0);
+                        break;
+                    case RIGHT:
+                        box.move(FIELD_SELL_SIZE, 0);
+                        break;
+                    case UP:
+                        box.move(0, -FIELD_SELL_SIZE);
+                        break;
+                    case DOWN:
+                        box.move(0, FIELD_SELL_SIZE);
+                        break;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion() {
+        for (Box box : gameObjects.getBoxes()) {
+            boolean isSet = false;
+            for (Home home : gameObjects.getHomes()) {
+                if (box.getX() == home.getX() && box.getY() == home.getY()) {
+                    isSet = true;
+                    break;
+                }
+            }
+            if (!isSet)
+                return;
+        }
+        eventListener.levelCompleted(currentLevel);
     }
 }
