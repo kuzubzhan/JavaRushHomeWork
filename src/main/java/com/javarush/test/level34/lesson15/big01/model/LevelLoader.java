8e387739ab1714b37bdfbf66a67c4d859e157775
frenchman <kujzub@gmail.kom>
2017-05-17 21:35:32 +0300

    level34.lesson15.big01_16-17

--- src/main/java/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
+++ src/main/java/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
@@ -1,25 +1,79 @@
 package com.javarush.test.level34.lesson15.big01.model;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LevelLoader {
+    private Path levels;
+
     public LevelLoader(Path levels) {
+        this.levels = levels;
     }
 
     public GameObjects getLevel(int level) {
-        Set<Wall> walls = new HashSet<>();
+        level = level > 60 ? level - 60 : level;
+        try (BufferedReader reader = new BufferedReader(new FileReader(levels.toFile()))) {
+            while (true) {
+                String line = reader.readLine();
+                if (line.startsWith("Maze")) {
+                    if (getNumber(line) == level) {
+                        int w = 0;
+                        int h = 0;
+                        for (int i = 0; i < 6; i++) {
+                            line = reader.readLine();
+                            if (i == 1) {
+                                w = getNumber(line);
+                            }
+                            if (i == 2) {
+                                h = getNumber(line);
+                            }
+                        }
+                         return createObjects(w, h, reader);
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public GameObjects createObjects(int sizeX, int sizeY, BufferedReader r) throws IOException {
         Set<Box> boxes = new HashSet<>();
+        Set<Wall> walls = new HashSet<>();
         Set<Home> homes = new HashSet<>();
-
-        walls.add(new Wall(20, 20));
-        walls.add(new Wall(30, 30));
-        walls.add(new Wall(40, 40));
-        boxes.add(new Box(50, 50));
-        homes.add(new Home(60, 60));
-        Player player = new Player(70, 70);
-
+        Player player = null;
+        for (int y = 0; y < sizeY; y++) {
+            char[] chars = r.readLine().toCharArray();
+            for (int x = 0; x < sizeX; x++) {
+                int newX = Model.FIELD_SELL_SIZE / 2 + x * Model.FIELD_SELL_SIZE;
+                int newY = Model.FIELD_SELL_SIZE / 2 + y * Model.FIELD_SELL_SIZE;
+                switch (chars[x]) {
+                    case 'X':
+                        walls.add(new Wall(newX, newY));
+                        break;
+                    case '*':
+                        boxes.add(new Box(newX, newY));
+                        break;
+                    case '.':
+                        homes.add(new Home(newX, newY));
+                        break;
+                    case '&':
+                        boxes.add(new Box(newX, newY));
+                        homes.add(new Home(newX, newY));
+                        break;
+                    case '@':
+                        player = new Player(newX, newY);
+                        break;
+                }
+            }
+        }
         return new GameObjects(walls, boxes, homes, player);
+    }
+
+    public int getNumber(String s) {
+        String[] split = s.split(":");
+        return Integer.parseInt(split[1].trim());
     }
 }
