a20376c3ba41198232197b99168c3c3f68cdd1a7
frenchman <kujzub@gmail.kom>
2017-05-23 01:02:08 +0300

    level36.lesson04.big01_6

--- src/main/java/com/javarush/test/level36/lesson04/big01/controller/Controller.java
+++ src/main/java/com/javarush/test/level36/lesson04/big01/controller/Controller.java
@@ -1,11 +1,13 @@
 package com.javarush.test.level36.lesson04.big01.controller;
 
 import com.javarush.test.level36.lesson04.big01.model.Model;
+import com.javarush.test.level36.lesson04.big01.view.EditUserView;
 import com.javarush.test.level36.lesson04.big01.view.UsersView;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -23,5 +25,9 @@ public class Controller {
 
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 }
--- src/main/java/com/javarush/test/level36/lesson04/big01/view/UsersView.java
+++ src/main/java/com/javarush/test/level36/lesson04/big01/view/UsersView.java
@@ -9,7 +9,11 @@ public class UsersView implements View {
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users:");
+        if (modelData.isDisplayDeletedUserList())
+            System.out.println("All deleted users:");
+        else
+            System.out.println("All users:");
+
         for (User user : modelData.getUsers()) {
             System.out.println("\t" + user);
         }
