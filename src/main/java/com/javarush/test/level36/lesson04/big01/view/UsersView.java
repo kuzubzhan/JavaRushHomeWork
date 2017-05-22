b5c827058a60df9a5ffa16e310f006b1e3199a9d
frenchman <kujzub@gmail.kom>
2017-05-22 23:28:44 +0300

    level36.lesson04.big01_3

---
+++ src/main/java/com/javarush/test/level36/lesson04/big01/view/UsersView.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level36.lesson04.big01.view;
+
+import com.javarush.test.level36.lesson04.big01.bean.User;
+import com.javarush.test.level36.lesson04.big01.controller.Controller;
+import com.javarush.test.level36.lesson04.big01.model.ModelData;
+
+public class UsersView implements View {
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (User user : modelData.getUsers()) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+}
