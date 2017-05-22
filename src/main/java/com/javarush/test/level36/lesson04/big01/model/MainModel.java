77a0a52cfffdf2ddcf0720251544157728569cda
frenchman <kujzub@gmail.kom>
2017-05-22 23:45:56 +0300

    level36.lesson04.big01_4

---
+++ src/main/java/com/javarush/test/level36/lesson04/big01/model/MainModel.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+import com.javarush.test.level36.lesson04.big01.model.service.UserService;
+import com.javarush.test.level36.lesson04.big01.model.service.UserServiceImpl;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+}
