98701278fa1c34e6c2a76527373e713b0beb6263
frenchman <kujzub@gmail.kom>
2017-05-26 22:44:22 +0300

    level27.lesson15.big01_13

--- src/main/java/com/javarush/test/level27/lesson15/big01/statistic/StatisticManager.java
+++ src/main/java/com/javarush/test/level27/lesson15/big01/statistic/StatisticManager.java
@@ -1,9 +1,16 @@
 package com.javarush.test.level27.lesson15.big01.statistic;
 
 import com.javarush.test.level27.lesson15.big01.statistic.event.EventDataRow;
+import com.javarush.test.level27.lesson15.big01.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
     private static StatisticManager ourInstance = new StatisticManager();
+    private StatisticStorage storage = new StatisticStorage();
 
     public static StatisticManager getInstance() {
         return ourInstance;
@@ -14,5 +21,16 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
         // TODO: 26.05.17
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> map;
+
+        public StatisticStorage() {
+            map = new HashMap<>();
+            for (EventType eventType : EventType.values()) {
+                map.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
     }
 }
