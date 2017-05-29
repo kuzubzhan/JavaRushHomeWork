ec247eb1a6be97c9815a51e6a13f521d2d8844a1
frenchman <kujzub@gmail.kom>
2017-05-29 14:49:58 +0300

    level37.lesson04.big01_4

---
+++ src/main/java/com/javarush/test/level37/lesson04/big01/FactoryProducer.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level37.lesson04.big01;
+
+import com.javarush.test.level37.lesson04.big01.female.FemaleFactory;
+import com.javarush.test.level37.lesson04.big01.male.MaleFactory;
+
+public class FactoryProducer {
+    public static AbstractFactory getFactory(HumanFactoryType type) {
+        switch (type) {
+            case MALE:
+                return new MaleFactory();
+            case FEMALE:
+                return new FemaleFactory();
+        }
+        return null;
+    }
+
+    public enum HumanFactoryType {
+        MALE,
+        FEMALE
+    }
+}
--- src/main/java/com/javarush/test/level37/lesson04/big01/Solution.java
+++ src/main/java/com/javarush/test/level37/lesson04/big01/Solution.java
@@ -1,10 +1,23 @@
 package com.javarush.test.level37.lesson04.big01;
 
-import com.javarush.test.level37.lesson04.big01.male.MaleFactory;
-
 public class Solution {
     public static void main(String[] args) {
-        MaleFactory factory = new MaleFactory();
+        AbstractFactory factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.FEMALE);
+        useFactory(factory);
+
+        factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.MALE);
+        useFactory(factory);
+/*
+Woman{}
+KidGirl{}
+TeenGirl{}
+Man{}
+KidBoy{}
+TeenBoy{}
+         */
+    }
+
+    public static void useFactory(AbstractFactory factory) {
         System.out.println(factory.getPerson(99));
         System.out.println(factory.getPerson(4));
         System.out.println(factory.getPerson(15));
